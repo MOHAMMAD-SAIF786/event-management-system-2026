89336f036b6f252fc01628c7519ece9f0697c79e
MOHAMMAD SAIF <mdsaif123654@gmail.com>
2026-08-17 13:04:51 +0530

    Serve media files in production

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from accounts.views import user_logout
 
 urlpatterns = [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('logout/', user_logout, name='root_logout'),
@@ -38,4 +45,3 @@ handler404 = 'home.views.custom_404'
 handler500 = 'home.views.custom_500'
 handler403 = 'home.views.custom_403'
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
